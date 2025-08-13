e58eb93ec794928918e4bb97714e98a3be4c8267
Viktoriya Mihova <135457634+vikimihova@users.noreply.github.com>
2025-08-13 22:59:57 +0300

    model: create database

--- Management_App_2025/ManagementApp.Data/ApplicationDbContext.cs
+++ Management_App_2025/ManagementApp.Data/ApplicationDbContext.cs
@@ -1,7 +1,24 @@
-﻿namespace ManagementApp.Data
-{
-    public class ApplicationDbContext
-    {
+﻿using ManagementApp.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
+namespace ManagementApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, Guid>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public virtual DbSet<Department> Departments { get; set; }
+
+        public virtual DbSet<JobTitle> JobTitles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
     }
 }
