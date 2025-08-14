e3d75564eeb29319f368539c85c20ca17dbbfbbd
Viktoriya Mihova <135457634+vikimihova@users.noreply.github.com>
2025-08-14 22:33:28 +0300

    build: add infrastructure layer with extension methods

---
+++ Management_App_2025/ManagementApp.Infrastructure/Extensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+
+using ManagementApp.Data;
+
+namespace ManagementApp.Infrastructure
+{
+    public static class Extensions
+    {
+        public static IApplicationBuilder ApplyMigrations(this IApplicationBuilder app)
+        {
+            using IServiceScope serviceScope = app.ApplicationServices.CreateScope();
+
+            ApplicationDbContext context = serviceScope.ServiceProvider.GetRequiredService<ApplicationDbContext>()!;
+            context.Database.MigrateAsync();
+
+            return app;
+        }
+    }
+}
