913dc0a3ddc617619829b80d69520c51635ff10a
Viktoriya Mihova <135457634+vikimihova@users.noreply.github.com>
2025-08-14 22:56:26 +0300

    build: add error messages to layer Common

---
+++ Management_App_2025/ManagementApp.Common/ErrorMessages.cs
@@ -0,0 +1,16 @@
+﻿namespace ManagementApp.Common
+{
+    public static class ErrorMessages
+    {
+        public static class Roles
+        {
+            public const string ErrorWhileCreatingRole = "Error occurred while creating the {0} role!";
+            public const string ErrorWhileAddingUserToRole = "Error occurred while adding the user {0} to the {1} role!";
+        }
+
+        public static class Services
+        {
+            public const string ErrorTryingToObtainService = "Service for {0} cannot be obtained!";
+        }
+    }
+}
