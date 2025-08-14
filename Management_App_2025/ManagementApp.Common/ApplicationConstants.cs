fdbf09492d4f1c2b00583a6754cf3cc2749ea4c2
Viktoriya Mihova <135457634+vikimihova@users.noreply.github.com>
2025-08-14 22:56:04 +0300

    build: add application constants

---
+++ Management_App_2025/ManagementApp.Common/ApplicationConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace ManagementApp.Common
+{
+    public static class ApplicationConstants
+    {
+        public const string EmployeeRoleName = "Employee";
+        public const string ManagerRoleName = "Manager";
+        public const string AdminRoleName = "HR Admin";
+    }
+}
