429efdc7dee8446b32868429270d74e75c6c7408
Viktoriya Mihova <135457634+vikimihova@users.noreply.github.com>
2025-08-17 16:23:17 +0300

    build: add extension method GetUserId

---
+++ Management_App_2025/ManagementApp.Infrastructure/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+
+namespace ManagementApp.Infrastructure
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        public static string? GetUserId(this ClaimsPrincipal? userClaimsPrincipal)
+        {
+            return userClaimsPrincipal?.FindFirst(ClaimTypes.NameIdentifier)?.Value ?? null;
+        }
+    }
+}
