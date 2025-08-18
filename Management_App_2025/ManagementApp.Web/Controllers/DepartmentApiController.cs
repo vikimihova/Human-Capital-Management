6429e51c2aeee87714727a9f2fd94b6ae3514600
Viktoriya Mihova <135457634+vikimihova@users.noreply.github.com>
2025-08-18 23:41:41 +0300

    build: add ApiController for Department features

---
+++ Management_App_2025/ManagementApp.Web/Controllers/DepartmentApiController.cs
@@ -0,0 +1,25 @@
+﻿using ManagementApp.Core.Services.Interfaces;
+using ManagementApp.Core.ViewModels.Department;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+using static ManagementApp.Common.ApplicationConstants;
+
+namespace ManagementApp.Web.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize(Roles = AdminRoleName)]
+    public class DepartmentApiController : ControllerBase
+    {
+        private readonly IDepartmentService departmentService;
+        private readonly ILogger<DepartmentApiController> logger;
+
+        public DepartmentApiController(IDepartmentService departmentService,
+            ILogger<DepartmentApiController> logger)
+        {
+            this.departmentService = departmentService;
+            this.logger = logger;
+        }
+    }
+}
